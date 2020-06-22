77502b21400b6d09f2cb344602549237f64c3c37
Jamison Griffith <jamison.griffith@gmail.com>
2020-06-22 17:21:55 -0600

    Added comments and a more user-friendly read-out

--- compliment.py
+++ compliment.py
@@ -1,5 +1,11 @@
+# This program is intended to intake a novel DNA sequence and then output it's complementary strand
+
+# Comment out below to test a smaller sample strand for accuracy.
 file = open('rosalind_revc.txt', 'r')
 fullSet = file.read()
+
+# The following line was used to test a smaller DNA strand, since it wasn't apparent whether the code was
+# fullSet = "AAAACCCGGT"
 
 reversedComp = ""
 
@@ -13,4 +19,7 @@ for letter in fullSet[::-1]:
     elif letter == 'G':
         reversedComp += 'C'
 
+print("Original DNA Sequence: ")
+print(fullSet)
+print("Complementary DNA Sequence: ")
 print(reversedComp)
